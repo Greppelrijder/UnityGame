dc614387e2f5ca132bc674ce2da9ab85abdea44f
glaizigekever <otisstroo@gmail.com>
2023-07-01 20:50:49 +0200

    5c

--- FirstGame/Assets/playermovement.cs
+++ FirstGame/Assets/playermovement.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class playermovement : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = transform.position.z;
-        transform.position = Vector3.MoveTowards(transform.position, mousePosition, 5)
-         
+        
+         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         mousePosition.z = transform.position.z;
+         //transform.position = Vector3.MoveTowards(transform.position, mousePosition, 1);
+         transform.position = mousePosition;
     }
 }
